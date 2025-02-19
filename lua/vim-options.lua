9c848353f2d5df5e8b750c0b5b5b54907877002a
FADiamond <felixantoinediamond@icloud.com>
2025-02-18 23:03:53 -0500

    First functionnal version for Lua and partial version for python and js

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,8 +1,23 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.g.mapleader = ' '
-vim.g.localleader = ' '
-vim.opt.number = true
+vim.g.mapleader = " "
+vim.g.localleader = " "
+vim.g.have_nerd_fond = true
 
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.showmode = false
+vim.opt.breakindent = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.signcolumn = "yes" -- Keep signcolumn on by default
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 1000
+vim.opt.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+vim.opt.inccommand = 'split'
+vim.opt.cursorline = true
+vim.opt.scrolloff = 10
+
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
